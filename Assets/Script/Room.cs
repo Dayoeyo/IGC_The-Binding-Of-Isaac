b2de3663ace38f3a42cfcc79f976e8782cb10af1
kaffu0424 <kaffu@naver.com>
2023-09-08 18:04:20 +0900

    MapGenerator, GameManager, Room
    Room 작성
    GameManager 작성
    MapGenerator 작성

--- Assets/Script/Room.cs
+++ Assets/Script/Room.cs
@@ -2,24 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room 
+public class Room
 {
     /* roomNumber
      * 0 : 배정되지않은곳
-     * 1 : 임시 배정
+     * 1 : 일반방
      * 2 : 시작방
-     * 3 : 일반방
-     * 4 : 보스방
-     * 5 : 상점방
-     * 6 : 황금방
-     * 7 : 달방
+     * 3 : 보스방
+     * 4 : 상점방
+     * 5 : 황금방
+     * 6 : 달방
      * roomNumber 변수의 수치에 맞춰서 방 오브젝트 배치.
      */
-    int roomNumber; // 방 종류 번호
-    int isClear; // 클리어 여부
 
-    Room(int _number) // Room 생성자
+    private int roomNumber; // 방 종류 번호
+    public int RoomNumber
+    { get { return roomNumber; } }
+    private bool isClear; // 방 클리어 여부
+    public bool IsClear
+    { get { return isClear; } }
+
+    public Room(int _number , bool _clear) // Room 생성자
     {
         roomNumber = _number;
+        isClear = _clear;
+    }
+
+    public void ResetRoom() // Room 초기화
+    {
+        roomNumber = 0;
+        isClear = false;
+    }
+
+    public void ChangeRoom(int after)
+    {
+        roomNumber = after;
     }
 }
